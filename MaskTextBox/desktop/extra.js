022c7713ead0440f5c4df2c71835517f6df7faf0
Felipe Guarneri <felipe@wikicode.com.br>
2019-03-10 22:22:29 -0300

    Maxk

---
+++ MaskTextBox/desktop/extra.js
@@ -0,0 +1,10 @@
+(function(f) {
+    var c = ["enable", "disable", "flushCache", "trigger", "display", "close"],
+        b = this,
+        d = "autocompletecustom";
+
+    f.fn.Roda = function() {
+        bizagi.log ('string trace xxxxx' );
+        return "teste";
+    }
+}(jQuery));
